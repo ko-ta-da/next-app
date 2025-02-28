d6bf97bb8593bc9b9946677c619897d4fc548d8c
ko-ta-da <siam.s.kk@gmail.com>
2025-03-01 08:01:22 +0900

    修正７

--- app/news/search/page.tsx
+++ app/news/search/page.tsx
@@ -3,16 +3,21 @@ import { NEWS_LIST_LIMIT } from "@/app/_components/_constants";
 import NewsList from "@/app/_components/NewsList";
 import SearchField from "@/app/_components/SearchField";
 
-type Props = {
-    searchParams: {
-        q?: string;
-    };
-};
+// type Props = {
+//     searchParams: {
+//         q?: string;
+//     };
+// };
+
+interface Props {
+    searchParams: Promise<{ q?: string }>;
+}
 
 export default async function Page({ searchParams }: Props) {
+    const { q } = await searchParams;
     const { contents: news } = await getNewsList({
         limit: NEWS_LIST_LIMIT,
-        q: searchParams.q,
+        q,
     });
 
     return (

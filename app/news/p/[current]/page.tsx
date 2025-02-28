9f79023029ff54dea6ba0eab369040bb5221095e
ko-ta-da <siam.s.kk@gmail.com>
2025-03-01 07:37:13 +0900

    修正５

--- app/news/p/[current]/page.tsx
+++ app/news/p/[current]/page.tsx
@@ -3,23 +3,22 @@ import NewsList from "@/app/_components/NewsList";
 import Pagination from "@/app/_components/Pagination";
 import { NEWS_LIST_LIMIT } from "@/app/_components/_constants";
 
-type Props = {
-    params: {
-        current: string;
-    }
-}
+interface Props {
+    params: Promise<{ current: string }>;
+};
 
 export default async function Page({ params }: Props) {
-    const current = parseInt(params.current, 10);
+    const { current } = await params;
+    const currentNum = parseInt(current, 10);
 
     const { contents: news, totalCount } = await getNewsList({
         limit: NEWS_LIST_LIMIT,
-        offset: NEWS_LIST_LIMIT * (current - 1),
+        offset: NEWS_LIST_LIMIT * (currentNum - 1),
     });
     return (
         <>
             <NewsList news = {news} />
-            <Pagination totalCount = {totalCount} current = {current} />
+            <Pagination totalCount = {totalCount} current = {currentNum} />
         </>
 
     )
